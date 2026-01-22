5a7462b468ec123f197fb275099d83c9de72f349
duongbdh <duongbdhe182316@fpt.edu.vn>
2026-01-22 15:35:17 +0700

    chore: add base calculator structure

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function plus(a, b) {
+    return 0;
+}
+
+function minus(a, b) {
+    return 0;
+}
+
+function multiply(a, b) {
+    return 0;
+}
+
+function divide(a, b) {
+    return 0;
+}
+
+module.exports = { plus, minus, multiply, divide };
